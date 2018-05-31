9d8fe5dbf80a16ca9451ffa03c9c1ff7836206bb
liuyuchao <1020269358@qq.com>
2018-05-31 12:03:41 +0800

    add sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,7 @@
 <template>
-  <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  <text class="message">Now, let's use Vue.js to build your Weex app.  sdfjsdfkljsdkfljsd </text>
 </template>
+<style lang="sass" scoped>
+  .message { color: red; }
+</style>
+
